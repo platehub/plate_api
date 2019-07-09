690d98620dc2019c7631e59d33d5fa61ff30c8d6
Harmen Fuite <fuiteharmen@gmail.com>
2019-07-09 12:09:16 +0200

    Bundle install locally

---
+++ spec/classes/plate_object/partner_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe PlateApi::PlateObject::Partner do
+
+  it_behaves_like "a PlateObject" do
+    let(:subject) { described_class.new(1, {"name" => "A Name"}, {}) }
+  end
+
+  describe "#new" do
+    it "returns an instance of #{described_class}" do
+      expect(described_class.new(1, {"name" => "A Name"}, {})).to be_a described_class
+    end
+  end
+
+  describe "#name" do
+    subject { described_class.new(1, {"name" => "My Name"}, {})}
+    it "returns name as set in the attributes" do
+      expect(subject.name).to eq "My Name"
+    end
+  end
+
+  include_examples("has api_name", "partners") 
+
+
+end
